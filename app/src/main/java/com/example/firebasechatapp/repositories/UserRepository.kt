e4dbc491aeee728c10e062e01b7cd28a3c2662ff
nathalie7980 <londonhippos@gmail.com>
2023-03-27 14:50:25 +0800

    added Users fetching from Firebase in UserRepository

--- app/src/main/java/com/example/firebasechatapp/repositories/UserRepository.kt
+++ app/src/main/java/com/example/firebasechatapp/repositories/UserRepository.kt
@@ -1,9 +1,18 @@
 package com.example.firebasechatapp.repositories
 
 import com.example.firebasechatapp.data.model.User
+import com.google.firebase.firestore.CollectionReference
+import kotlinx.coroutines.tasks.await
 
-class UserRepository {
-    suspend fun getUserByEmail(email: String): User {
-        TODO()
+class UserRepository(private val ref: CollectionReference) {
+    suspend fun getUsers(): List<User> {
+        val res = ref.get().await()
+        val users = mutableListOf<User>()
+        res.documents.forEach {
+            it.toObject(User::class.java)?.let { user ->
+                users.add(user)
+            }
+        }
+        return users
     }
 }
